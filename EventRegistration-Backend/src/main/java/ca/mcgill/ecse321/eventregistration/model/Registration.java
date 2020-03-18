de753ac80ec1edcc3721041a70e7bc720c52c02b
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-18 11:33:56 -0400

    fixed identations in domain model

--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/Registration.java
@@ -6,35 +6,35 @@ import javax.persistence.Id;
 
 @Entity
 public class Registration{
-private Person person;
+	private Person person;
 
-@ManyToOne(optional=false)
-public Person getPerson() {
-   return this.person;
+	@ManyToOne(optional=false)
+	public Person getPerson() {
+		return this.person;
+	}
+
+	public void setPerson(Person person) {
+		this.person = person;
+	}
+
+	private Event event;
+
+	@ManyToOne(optional=false)
+	public Event getEvent() {
+		return this.event;
+	}
+
+	public void setEvent(Event event) {
+		this.event = event;
+	}
+
+	private Integer id;
+
+	public void setId(Integer value) {
+		this.id = value;
+	}
+	@Id
+	public Integer getId() {
+		return this.id;
+	}
 }
-
-public void setPerson(Person person) {
-   this.person = person;
-}
-
-private Event event;
-
-@ManyToOne(optional=false)
-public Event getEvent() {
-   return this.event;
-}
-
-public void setEvent(Event event) {
-   this.event = event;
-}
-
-private Integer id;
-
-public void setId(Integer value) {
-this.id = value;
-    }
-@Id
-public Integer getId() {
-return this.id;
-       }
-   }
--- EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
+++ EventRegistration-Backend/src/main/java/ca/mcgill/ecse321/eventregistration/model/RegistrationManager.java
@@ -8,46 +8,46 @@ import javax.persistence.Id;
 
 @Entity
 public class RegistrationManager{
-private Set<Person> person;
+	private Set<Person> person;
 
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Person> getPerson() {
-   return this.person;
+	@OneToMany(cascade={CascadeType.ALL})
+	public Set<Person> getPerson() {
+		return this.person;
+	}
+
+	public void setPerson(Set<Person> persons) {
+		this.person = persons;
+	}
+
+	private Set<Registration> registration;
+
+	@OneToMany(cascade={CascadeType.ALL})
+	public Set<Registration> getRegistration() {
+		return this.registration;
+	}
+
+	public void setRegistration(Set<Registration> registrations) {
+		this.registration = registrations;
+	}
+
+	private Set<Event> event;
+
+	@OneToMany(cascade={CascadeType.ALL})
+	public Set<Event> getEvent() {
+		return this.event;
+	}
+
+	public void setEvent(Set<Event> events) {
+		this.event = events;
+	}
+
+	private Integer id;
+
+	public void setId(Integer value) {
+		this.id = value;
+	}
+	@Id
+	public Integer getId() {
+		return this.id;
+	}
 }
-
-public void setPerson(Set<Person> persons) {
-   this.person = persons;
-}
-
-private Set<Registration> registration;
-
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Registration> getRegistration() {
-   return this.registration;
-}
-
-public void setRegistration(Set<Registration> registrations) {
-   this.registration = registrations;
-}
-
-private Set<Event> event;
-
-@OneToMany(cascade={CascadeType.ALL})
-public Set<Event> getEvent() {
-   return this.event;
-}
-
-public void setEvent(Set<Event> events) {
-   this.event = events;
-}
-
-private Integer id;
-
-public void setId(Integer value) {
-this.id = value;
-    }
-@Id
-public Integer getId() {
-return this.id;
-       }
-   }
