8894a86bce6ecf385da5712d8aefffcce9eed72a
mohamed-youssef-bou <mohamedyoussefbouaouina@gmail.com>
2020-03-20 11:16:56 -0400

    Adding route to the EventRegistration component

--- EventRegistration-Frontend/src/router/index.js
+++ EventRegistration-Frontend/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Hello from '@/components/Hello'
+import EventRegistration from "@/components/EventRegistration";
+
 
 Vue.use(Router)
 
@@ -10,6 +12,11 @@ export default new Router({
       path: '/',
       name: 'Hello',
       component: Hello
+    },
+    {
+      path: '/app',
+      name: 'EventRegistration',
+      component: EventRegistration
     }
   ]
 })
